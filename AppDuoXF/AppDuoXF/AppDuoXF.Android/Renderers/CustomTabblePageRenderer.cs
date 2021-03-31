9d65b8b78eb61391fb9db84df9f9a3b68b21bab0
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-03-31 11:36:35 -0300

    Customizing Height of the TabblePage for Android.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF.Android/Renderers/CustomTabblePageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using AppDuoXF.Droid.Renderers;
+using Google.Android.Material.BottomNavigation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabblePageRenderer))]
+namespace AppDuoXF.Droid.Renderers
+{
+    public class CustomTabblePageRenderer : TabbedPageRenderer
+    {
+        public CustomTabblePageRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+            
+            if(e.NewElement != null)
+            {
+                var relativeLayout = base.GetChildAt(0) as Android.Widget.RelativeLayout;
+                var bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
+                bottomNavigationView.SetMinimumHeight(300);
+            }
+            
+        }
+    }
+}
