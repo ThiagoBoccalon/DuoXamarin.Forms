471d64458d747f60cf2fb3c0b7859cca85072710
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-03-31 10:38:58 -0300

    Customizing height of the TabblePage for iOS.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF.iOS/Renderers/CustomTabblePageRenderer.cs
@@ -0,0 +1,23 @@
+﻿using AppDuoXF.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabblePageRenderer))]
+namespace AppDuoXF.iOS.Renderers
+{
+    public class CustomTabblePageRenderer : TabbedRenderer
+    {      
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+                        
+            var tabFrame = TabBar.Frame;
+
+            var tabHeight = 150;
+            tabFrame.Height = tabHeight;
+            tabFrame.Y = View.Frame.Height - tabHeight;
+
+            TabBar.Frame = tabFrame;
+        }
+    }
+}
