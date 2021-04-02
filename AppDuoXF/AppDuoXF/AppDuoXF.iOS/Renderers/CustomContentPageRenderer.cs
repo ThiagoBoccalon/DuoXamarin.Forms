4b412c324d62e32ceed2efe22c97e54778605df3
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-04-02 11:16:00 -0300

    Config status bar for iOS.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF.iOS/Renderers/CustomContentPageRenderer.cs
@@ -0,0 +1,22 @@
+﻿using AppDuoXF.iOS.Renderers;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CustomContentPageRenderer))]
+namespace AppDuoXF.iOS.Renderers
+{
+    public class CustomContentPageRenderer : PageRenderer
+    {
+        public override bool PrefersStatusBarHidden()
+        {
+            return true;
+        }
+
+    }
+}
