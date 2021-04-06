57bccd9040584973ce0668364f7cf52cb35b472d
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-04-05 21:39:20 -0300

    load lessons in viewmodel.

--- AppDuoXF/AppDuoXF/AppDuoXF/ViewModels/LessonsViewViewModel.cs
+++ AppDuoXF/AppDuoXF/AppDuoXF/ViewModels/LessonsViewViewModel.cs
@@ -1,25 +1,45 @@
 ﻿using AppDuoXF.Interfaces;
+using AppDuoXF.Models;
 using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AppDuoXF.ViewModels
 {
-    public class LessonsViewViewModel : BindableBase
+    public class LessonsViewViewModel : BindableBase, IInitialize
     {
+        private readonly ILessonService _lessonService;
         public ICommand NavigateToTrainingCommand { get; private set; }
+        public IList<LessonGroup> LessonGroup { get; private set; }
 
         public LessonsViewViewModel(ILessonService lessonService)
         {
+            _lessonService = lessonService;
             NavigateToTrainingCommand = new DelegateCommand(NavigateToTrainingExecute);
+            LessonGroup = new List<LessonGroup>();
         }
         
         private void NavigateToTrainingExecute()
         {
             System.Diagnostics.Debug.WriteLine("The button FAB was clicked.");
+        }
+
+        public async void Initialize(INavigationParameters parameters)
+        {
+            var groups = await GetLessonsGroup();
+
+            foreach (var group in groups)
+                LessonGroup.Add(group);
+        }
+
+        private async Task<IList<LessonGroup>> GetLessonsGroup()
+        {
+            return await _lessonService.GetLessonsGroup();
         }
     }
 }
