47bd9632f34cb2e95e997d18c633e4103d886979
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-04-13 22:04:07 -0300

    Convert image gray crown  to crown with level.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF/Converters/LevelToImageCrownConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppDuoXF.Converters
+{
+    public class LevelToImageCrownConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var level = value as string;
+
+            if (level == string.Empty)
+                return "crown_gray_stroke";
+
+            return "crown_stroke";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
