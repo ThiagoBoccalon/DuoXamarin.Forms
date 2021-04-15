3de92a83fb6dc0645bcb764bfeb50342d83cf4d3
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-04-15 00:56:14 -0300

    Create controls nativo of iOS for progress bar circle.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF.iOS/Controls/CircularProgressBariOS.cs
@@ -0,0 +1,47 @@
+﻿using CoreAnimation;
+using CoreGraphics;
+using System;
+using UIKit;
+
+namespace AppDuoXF.iOS.Controls
+{
+    public class CircularProgressBariOS : UIView
+    {
+        private CAShapeLayer progressLyr = new CAShapeLayer();
+        private CAShapeLayer trackLyr = new CAShapeLayer();
+
+        public CircularProgressBariOS(double width, double height)
+        {
+            MakeCircularPath(width, height);
+        }
+
+        private void MakeCircularPath(double width, double height)
+        {
+            BackgroundColor = UIColor.Clear;
+            Layer.CornerRadius = (nfloat)(width / 2);
+
+            var circlePath = new UIBezierPath();
+            circlePath.AddArc(
+                center: new CGPoint(x: width / 2, y: height / 2),
+                radius: (nfloat)((width - 1.5) / 2),
+                startAngle: (nfloat)(-0.5 * Math.PI),
+                endAngle: (nfloat)(1.5 * Math.PI),
+                clockWise: true
+            );
+
+            trackLyr.Path = circlePath.CGPath;
+            trackLyr.FillColor = UIColor.Clear.CGColor;
+            trackLyr.StrokeColor = UIColor.Blue.CGColor;
+            trackLyr.LineWidth = (nfloat)5.0;
+            trackLyr.StrokeEnd = (nfloat)1.0;
+            Layer.AddSublayer(trackLyr);
+
+            progressLyr.Path = circlePath.CGPath;
+            progressLyr.FillColor = UIColor.Clear.CGColor;
+            progressLyr.StrokeColor = UIColor.Red.CGColor;
+            progressLyr.LineWidth = (nfloat)5.0;
+            progressLyr.StrokeEnd = (nfloat)0.2;
+            Layer.AddSublayer(progressLyr);
+        }
+    }
+}
--- AppDuoXF/AppDuoXF/AppDuoXF.iOS/Renderers/CircularProgressBarRenderer.cs
+++ AppDuoXF/AppDuoXF/AppDuoXF.iOS/Renderers/CircularProgressBarRenderer.cs
@@ -1,17 +1,29 @@
 ﻿using AppDuoXF.Controls;
+using AppDuoXF.iOS.Controls;
 using AppDuoXF.iOS.Renderers;
-using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer(typeof(CircularProgressBar), typeof(CircularProgressBarRenderer))]
 namespace AppDuoXF.iOS.Renderers
 {
-    class CircularProgressBarRenderer : ViewRenderer<CircularProgressBar, UIView>
+    class CircularProgressBarRenderer : ViewRenderer<CircularProgressBar, CircularProgressBariOS>
     {
         protected override void OnElementChanged(ElementChangedEventArgs<CircularProgressBar> e)
         {
             base.OnElementChanged(e);
+            if(Control is null)
+            {
+                if (Element is null)
+                    return;
+
+                var nativeControl = new CircularProgressBariOS(
+                    Element.WidthRequest,
+                    Element.HeightRequest
+                    );
+
+                SetNativeControl(nativeControl);
+            }
         }
     }
 }
