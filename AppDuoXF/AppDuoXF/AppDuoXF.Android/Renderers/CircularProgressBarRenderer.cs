96513b84ba6ae14b90c8cfe42115227071c52e4d
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-04-25 14:05:19 -0300

    Create custom render of circle progress bar for Android.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF.Android/Renderers/CircularProgressBarRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppDuoXF.Controls;
+using AppDuoXF.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(CircularProgressBar), typeof(CircularProgressBarRenderer))]
+namespace AppDuoXF.Droid.Renderers
+{
+    public class CircularProgressBarRenderer : ViewRenderer<CircularProgressBar, Android.Widget.ProgressBar>
+    {
+        public CircularProgressBarRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CircularProgressBar> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
