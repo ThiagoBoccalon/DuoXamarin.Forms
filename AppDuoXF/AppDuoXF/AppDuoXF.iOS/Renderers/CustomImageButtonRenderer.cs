bff3f857e38ddfe7eec119a4add584b67deedd45
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-04-29 11:59:37 -0300

    Remove status of select for ImageButtons in iOS.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF.iOS/Renderers/CustomImageButtonRenderer.cs
@@ -0,0 +1,29 @@
+﻿using AppDuoXF.iOS.Renderers;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ImageButton), typeof(CustomImageButtonRenderer))]
+namespace AppDuoXF.iOS.Renderers
+{
+    public class CustomImageButtonRenderer : ImageButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ImageButton> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                Control.AdjustsImageWhenHighlighted = false;
+        }
+
+        protected override UIButton CreateNativeControl()
+        {
+            return new UIButton(UIButtonType.Custom);
+        }
+    }
+}
