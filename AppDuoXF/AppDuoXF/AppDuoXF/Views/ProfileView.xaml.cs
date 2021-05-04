3d4e54cae2cf386233fa4481a6e4a4ca64bd71aa
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-05-04 10:33:24 -0300

    Create status of button for achievements and friends.

--- AppDuoXF/AppDuoXF/AppDuoXF/Views/ProfileView.xaml.cs
+++ AppDuoXF/AppDuoXF/AppDuoXF/Views/ProfileView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AppDuoXF.Interfaces;
 using AppDuoXF.Views.TitleViews;
+using System;
 using Xamarin.Forms;
 
 namespace AppDuoXF.Views
@@ -10,6 +11,72 @@ namespace AppDuoXF.Views
         public ProfileView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var index = 0;
+            foreach(var view in flexLayoutSection.Children)
+            {
+                if(view is Grid grid)
+                {
+                    if(index++ == 0)
+                    {
+                        GoToStateSelected(grid);
+                        continue;
+                    }
+
+                    GoToStateSelected(grid);
+                }
+            }
+        }
+
+        private Grid _lastSelected;
+
+        private void OnTappedSection(object sender, EventArgs args)
+        {
+            if(sender is Grid grid)
+            {
+                GoToStateUnSelected(_lastSelected);
+                GoToStateSelected(grid);
+
+                if(grid.AutomationId == "gridAchievements")
+                {
+                    System.Diagnostics.Debug.WriteLine("tapped achievements");
+                    return;
+                }
+
+                if (grid.AutomationId == "gridFriends")
+                {
+                    System.Diagnostics.Debug.WriteLine("tapped friends");
+                    return;
+                }
+
+            }
+        }
+
+        private void GoToStateSelected(Grid grid)
+        {
+            _lastSelected = grid;
+
+            if(grid.Children[0] is Label label &&
+                grid.Children[1] is BoxView boxView)
+            {
+                VisualStateManager.GoToState(label, "Selected");
+                VisualStateManager.GoToState(boxView, "Selected");
+            }
+        }
+
+        private void GoToStateUnSelected(Grid grid)
+        {
+            if(grid.Children[0] is Label label &&
+                grid.Children[1] is BoxView boxView)
+            {
+                VisualStateManager.GoToState(label, "UnSelected");
+                VisualStateManager.GoToState(boxView, "UnSelected");
+            }
         }
 
         public string GetIcon()
