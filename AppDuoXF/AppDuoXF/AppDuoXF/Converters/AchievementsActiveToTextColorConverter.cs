06ef1a4554e24cbb3b288f97db3652ab5d0a0d4f
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-06-15 09:05:42 -0300

    Convert achievement level color.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF/Converters/AchievementsActiveToTextColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppDuoXF.Converters
+{
+    class AchievementsActiveToTextColorConverter : IValueConverter
+    {
+        private Color _activeColor = Color.White;
+        private Color _finishColor = Color.FromHex("#cc7700");
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool isActive)
+            {
+                if (isActive)
+                    return _activeColor;
+
+                return _finishColor;
+            }
+
+            return _activeColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
