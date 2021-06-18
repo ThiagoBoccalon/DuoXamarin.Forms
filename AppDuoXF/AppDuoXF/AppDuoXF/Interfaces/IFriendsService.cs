0ddbd9e5ecf0b5a82a3fbec21c36422e4841b943
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-06-18 12:04:26 -0300

    Implement fake class with friends list.

---
+++ AppDuoXF/AppDuoXF/AppDuoXF/Interfaces/IFriendsService.cs
@@ -0,0 +1,13 @@
+﻿using AppDuoXF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDuoXF.Interfaces
+{
+    public interface IFriendsService
+    {
+        Task<IList<Friend>> GetFriends();
+    }
+}
---
+++ AppDuoXF/AppDuoXF/AppDuoXF/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDuoXF.Models
+{
+    public class Friend
+    {
+        public string Photo { get; set; }
+        public string Name { get; set; }
+        public string Experience { get; set; }
+    }
+}
