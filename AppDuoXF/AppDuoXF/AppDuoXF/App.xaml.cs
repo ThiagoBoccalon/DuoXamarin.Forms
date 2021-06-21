0b2084082ddb6de6f31f4f2d4dd923a24f80bf1f
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2021-06-21 09:55:50 -0300

    Correcting of the reference between interface and class friends.

--- AppDuoXF/AppDuoXF/AppDuoXF/App.xaml.cs
+++ AppDuoXF/AppDuoXF/AppDuoXF/App.xaml.cs
@@ -41,6 +41,7 @@ namespace AppDuoXF
             containerRegistry.Register<ILessonService, LessonServiceFake>();
             containerRegistry.Register<IStoriesService, StoriesServiceFake>();
             containerRegistry.Register<IAchievementsService, AchievementsServiceFake>();
+            containerRegistry.Register<IFriendsService, FriendsServiceFake>();
         }
     }
 }
