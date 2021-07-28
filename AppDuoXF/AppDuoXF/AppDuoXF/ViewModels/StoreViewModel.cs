4980c39a2255106b289f8cba19ec47b622c413a9
Thiago Bucalon <thiago_rodrigo90@hotmail.com>
2021-07-28 17:45:35 +0100

    Implement loading of store items in viewmodel.

--- AppDuoXF/AppDuoXF/AppDuoXF/ViewModels/StoreViewModel.cs
+++ AppDuoXF/AppDuoXF/AppDuoXF/ViewModels/StoreViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Prism.Commands;
+﻿using AppDuoXF.Interfaces;
+using AppDuoXF.Models;
+using Prism;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -6,11 +9,36 @@ using System.Linq;
 
 namespace AppDuoXF.ViewModels
 {
-    public class StoreViewModel : BindableBase
+    public class StoreViewModel : BindableBase, IActiveAware
     {
-        public StoreViewModel()
+        private readonly IStoreService _storeService;
+        
+        private bool _isActive;
+        public bool IsActive
         {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value, RaisePropertyChanged);
+        }
 
+        public event EventHandler IsActiveChanged;
+        public List<StoreItemGroup> Groups { get; private set; }
+
+        public StoreViewModel(IStoreService storeService)
+        {
+            _storeService = storeService;
+            Groups = new List<StoreItemGroup>();
+        }
+
+        private async void RaisePropertyChanged()
+        {
+            if (IsActive)
+            {
+                if (!Groups.Any())
+                {
+                    var storeGroups = await _storeService.GetItems();
+                    Groups.AddRange(storeGroups);
+                }
+            }
         }
     }
 }
